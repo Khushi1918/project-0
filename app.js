4d547f233982b7f0fd17599ba94a01a9d1ea81d0
Khushi1918 <btech10369.21@bitmesra.ac.in>
2023-07-03 17:18:26 +0530

    new changes

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express= require('express');
-const https= require('https');
 const bodyParser= require('body-parser');
 const ejs=require('ejs');
 const mongoose= require('mongoose');
 const bcrypt=require('bcrypt');
+const session = require('express-session');
 const app= express();
-
+const saltRounds = 10;
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+app.use(session({secret:'khushi_budhu_h'}));
+const fetch = require("node-fetch");
+
 mongoose.connect("mongodb://127.0.0.1:27017/recipeDB");
 const recipeSchema= new mongoose.Schema({
     title:String,
@@ -24,50 +27,45 @@ const loginSchema=new mongoose.Schema({
 const Recipe= mongoose.model('Recipe',recipeSchema);
 const Login=mongoose.model('Login',loginSchema);
 var log=false;
+console.log(log);
+
+
 app.get('/',function(req,res){
-    if(log==false){
+    if(!req.session.user_id){
     res.render('home',{change:"Signin"})
     }
     else{
         res.render('home',{change:"Logout"})
     }
 })
-app.get('/yourrecipe',function(req,res){
-    if(log == false){
-        res.redirect("/signin");
-    }
-    else{
-    res.render('yourrecipe',{change:"Logout"});
-    }
-})
+
+
 app.get('/posts',function(req,res){
     Recipe.find({},function(error,foundrecipe){
         if(error){
             console.log(error)
         }
         else{
-            if(log==false){
+            if(!req.session.user_id){
         res.render('posts',{recipePost:foundrecipe,change:"Signin"})
             }
             else{
                 res.render('posts',{recipePost:foundrecipe,change:"Logout"})
             }
         }
-
     })
-    
 })  
-app.get('/Logout',function(req,res){
-    log=false;
-    res.redirect('/');
+
+app.get('/yourrecipe',function(req,res){
+    if(!req.session.user_id){
+        res.redirect("/signin");
+    }
+    else{
+    res.render('yourrecipe',{change:"Logout"});
+    }
 })
 
-
 app.post('/yourrecipe',function(req,res){
-   
-    
-
-
     var head=req.body.title;
     var ind=req.body.ingrediants;
     var desp=req.body.description;
@@ -77,97 +75,96 @@ app.post('/yourrecipe',function(req,res){
         description:desp
     });
     newrecipe.save();
-    //console.log(newrecipe.title);
     res.redirect('/posts')
 });
 
-/*app.get('/recipe',function(req,res){
-    const url="https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata"
-    https.get(url,function(response){
-        console.log(response);
-        response.on("data",function(data){
-            const rec= JSON.parse(data)
-            const dish= rec.meals[0].strMeal
-            console.log(dish)
-            
-        })
-        
-    })
-
-})*/
 var dishname="";
 var dishcategory = "";
 var dishArea="";
 var dishinst="";
 var image="";
 var youtube="";
+
 app.get('/recipe', function(req, res) {
     res.render('recipe');
-  });
-  app.get('/recipeview',function(req,res){
-    res.render('recipeview',{name:dishname,cat:dishcategory,org:dishArea,instruct:dishinst,pic:image,link:youtube});
-  })
+});
+
+
 app.post('/recipe',function(req,res){
     const query=req.body.recipe;
     const url = "https://www.themealdb.com/api/json/v1/1/search.php?s="+query+"";
-    https.get(url, function(response) {
-      let data = '';
-  
-      response.on("data", function(chunk) {
-        data += chunk;
-      });
-  
-      response.on("end", function() {
-        const rec = JSON.parse(data);
-         dishname = rec.meals[0].strMeal;
+    fetch(url)
+    .then((response) => response.json())
+    .then((rec)=>{
+        // console.log(rec);
+        dishname = rec.meals[0].strMeal;
          dishcategory = rec.meals[0].strCategory;
          dishArea = rec.meals[0].strArea;
          dishinst = rec.meals[0].strInstructions;
          image=rec.meals[0].strMealThumb;
          youtube=rec.meals[0].strYoutube;
-        res.redirect('/recipeview');
-
-        
-      });
+         res.redirect('/recipeview');
     });
 })
 
-app.get('/signin',function(req,res){
-    res.render('signin');
 
+app.get('/recipeview',function(req,res){
+    res.render('recipeview',{name:dishname,cat:dishcategory,org:dishArea,instruct:dishinst,pic:image,link:youtube});
 })
+
+app.get('/signin',function(req,res){
+    res.render('Signin');
+})
+
 app.post('/signin',function(req,res){
     const uname=req.body.name;
     const upass=req.body.pass;
-    Login.findOne({username:uname},function(err,founduser){
+    Login.findOne({name:uname},function(err,founduser){
         if(err){
             console.log(err);
         }
         else if(founduser){
-            const uhashpass=bcrypt.compare(upass,founduser.password);
-            if(uhashpass == upass ){
-                log=true;
-                res.redirect('/yourrecipe');
-            }
+            bcrypt.compare(upass, founduser.password,function(err, result)
+            {
+                if(result ==1){
+                    req.session.user_id = founduser.id;
+                    res.redirect('/yourrecipe');
+                }
+                else{
+                    res.redirect('/signin');
+                }
+            });
         }
     })
-
 })
 
 app.get('/signup',function(req,res){
     res.render('signup');
 })
+
 app.post('/signup',function(req,res){
     const data= req.body.rpass;
-    const hashpass= bcrypt.hash(data,12);
-    const newuser= new Login({
-        email:req.body.remail,
-        name:req.body.rname,
-        password:hashpass
-    })
-    newuser.save();
-
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+        bcrypt.hash(data, salt, function(err, hash) {
+            const newuser= new Login({
+                email:req.body.remail,
+                name:req.body.rname,
+                password:hash
+            })
+            newuser.save();
+            req.session.user_id = newuser.id;
+            res.redirect('/');
+        });
+    });
+    
+    
 })
+
+app.get('/Logout',function(req,res){
+    req.session.destroy();
+    res.redirect('/');
+})
+
 
 app.listen(3000,function(req,res){
     console.log("Server running at 3000");
